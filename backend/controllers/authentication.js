4775da69f9606f9fbae792a60bccf58935aa0d5b
Crisila <crisilaaban@gmail.com>
2024-03-04 16:53:57 -0800

    complete

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -5,7 +5,23 @@ const bcrypt = require('bcrypt');
 const { User } = db;
 
 router.post('/', async (req, res) => {
-     console.log('IN HERE');
+     try {
+          let user = await User.findOne({
+               where: { email: req.body.email }
+          });
+
+          if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+               res.status(404).json({
+                    message: `Could not find a user with the provided username and password`
+               });
+          } else {
+               res.json({ user });
+          }
+     } catch (error) {
+          // Handle errors
+          console.error("Error:", error);
+          res.status(500).json({ message: "Internal Server Error" });
+     }
 });
 
 module.exports = router;
