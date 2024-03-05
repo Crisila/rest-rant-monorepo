907b1c7e1c0d7204324016ed89639e29043c3c5d
Crisila <crisilaaban@gmail.com>
2024-03-04 21:01:29 -0800

    complete using JWT

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const db = require("../models");
 const bcrypt = require('bcrypt');
+const jwt = require('json-web-token');
 
 const { User } = db;
 
@@ -18,8 +19,8 @@ router.post('/', async (req, res) => {
                     message: `Could not find a user with the provided username and password`
                });
           } else {
-               req.session.userId = user.userId;
-               res.json({ user });
+               const result = await jwt.encode(process.env.JWT_SECRET, {id: user.userId})
+               res.json({user: user, token: result.value})
           }
      } catch (error) {
           // Handle errors
@@ -29,21 +30,37 @@ router.post('/', async (req, res) => {
 });
 
 
+
 router.get('/profile', async (req, res) => {
-     console.log(req.session.userId);
      try {
-          let user = await User.findOne({
-               where: {
-                    userId: req.session.userId
-               }
-          });
-          res.json(user);
+          // Split the authorization header into [ "Bearer", "TOKEN" ]:
+          const [authenticationMethod, token] = req.headers.authorization.split(' ');
+
+          // Only handle "Bearer" authorization for now 
+          //  (we could add other authorization strategies later):
+          if (authenticationMethod == 'Bearer') {
+
+               // Decode the JWT
+               const result = await jwt.decode(process.env.JWT_SECRET, token);
+
+               // Get the logged in user's id from the payload
+               const { id } = result.value;
+
+               // Find the user object using their id:
+               let user = await User.findOne({
+                    where: {
+                         userId: id
+                    }
+               });
+               res.json(user);
+          }
      } catch {
           res.json(null);
      }
-});
+})
 
 
+// BONUS
 router.post('/super-important-route', async (req, res) => {
      if (req.session.userId) {
           console.log('Do the really super important thing');
